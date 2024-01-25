006eb963caef298e61e43c1c788aefe29d7590f0
Ljubow Neumann <citymnk@gmail.com>
2024-01-25 21:31:57 +0100

    Make user form, checks for non-empty values before sending data

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <input type="text" v-model="userName" placeholder="Name">
+  <input type="password" v-model="userPass" placeholder="Password">
+  <input type="email" v-model="userEmail" placeholder="Email">
+  <p className="error">{{ error }}</p>
+  <button @click="sendData()">Send</button>
+
+  <div v-for="(el, index) in this.users" :key="index">
+    <p>{{ el.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: [],
+      error: '',
+      userName: '',
+      userPass: '',
+      userEmail: ''
+    }
+  },
+  methods: {
+    sendData() {
+      if (this.userName === '') {
+        this.error = 'Name was not set';
+        return;
+      } else if (this.userEmail === '') {
+        this.error = 'Email was not set';
+        return;
+      } else if (this.userPass === '') {
+        this.error = 'Pass was not set';
+        return;
+      }
+
+      this.error = '';
+
+      this.users.push({
+        name: this.userName,
+        pass: this.userPass,
+        email: this.userEmail
+      })
+      this.userName = ''
+      this.userPass = ''
+      this.userEmail = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+  input {
+    display: block;
+    margin-bottom: 10px;
+    border-radius: 3px;
+    border: 1px solid silver;
+    outline: none;
+    padding: 10px 15px;
+    background: #fafafa;
+    color: #333;
+  }
+
+  button {
+    border: 0;
+    border-radius: 5px;
+    outline: none;
+    padding: 10px 15px;
+    background: #6cd670;
+    color: #167f3d;
+    font-weight: bold;
+    cursor: pointer;
+    transition: transform 500ms ease;
+  }
+
+  button:hover {
+    transform: translateY(-5px);
+  }
+
+  .user {
+    width: 500px;
+    margin-top: 20px;
+    border: 1px solid silver;
+    background: #e3e3e3;
+    color: #222;
+    padding: 20px;
+    border-radius: 5px;
+  }
+</style>
